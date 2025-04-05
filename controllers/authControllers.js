0ba74b9f80555a3a6db3d0fea05efe244720e853
chandan083 <code.us.cm@gmail.com>
2025-04-05 12:27:33 +0530

    Add login functionality with JWT authentication and update environment variables

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,6 +5,7 @@ const {
   handleInternalServerError,
 } = require("../utils/handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   register: async (req, res) => {
@@ -29,6 +30,38 @@ module.exports = {
       return handleSuccess(res, 200, "User registered successfully", user);
     } catch (error) {
       console.error("Error registering user:", error);
+      return handleInternalServerError(res);
+    }
+  },
+  Login: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) return handleError(res, 400, "Email is required");
+    if (!password) return handleError(res, 400, "Password is required");
+    if (password.length < 6)
+      return handleError(
+        res,
+        400,
+        "Password must be at least 6 characters long"
+      );
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return handleError(res, 400, "Email not found");
+
+      const matched = bcrypt.compareSync(password, user.password);
+      if (!matched) return handleError(res, 400, "Email or password is wrong");
+
+      // Generate JWT
+      const token = jwt.sign(
+        { id: user._id, email: user.email, name: user.name },
+        process.env.JWT_SECRET,
+        { expiresIn: "2h" } 
+      );
+
+      return handleSuccess(res, 200, "Login successful", { user, token });
+    } catch (error) {
+      console.error("Error logging in user:", error);
       return handleInternalServerError(res);
     }
   },
