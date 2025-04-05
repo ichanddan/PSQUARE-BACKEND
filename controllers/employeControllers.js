374b8db67c579135a9f96b716a657dee1c3e2a06
chandan083 <code.us.cm@gmail.com>
2025-04-05 17:37:47 +0530

    Enhance employee management functionality with add, update, fetch, and delete operations; improve error handling and success responses

--- controllers/employeControllers.js
+++ controllers/employeControllers.js
@@ -4,18 +4,48 @@ const {
   handleSuccess,
   handleInternalServerError,
 } = require("../utils/handler");
-const bcrypt = require("bcrypt");
 
 module.exports = {
   Add: async (req, res) => {
     try {
       const cheekEmail = await employee.findOne({ email });
       if (cheekEmail) return handleError(res, 400, "Email already exists");
-      const user = await employee.create(req.body);
-      if (!user) return handleError(res, 400, "Employee registration failed");
-      return handleSuccess(res, 200, "Employee registered successfully", user);
+      const Employee = await employee.create(req.body);
+      if (!Employee) return handleError(res, 400, "Employee registration failed");
+      return handleSuccess(res, 200, "Employee registered successfully", Employee);
     } catch (error) {
-      console.error("Error registering user:", error);
+      console.error("Error registering Employee:", error);
+      return handleInternalServerError(res);
+    }
+  },
+  GetAllEmployee: async (req, res) => {
+    try {
+      const data = await employee.find();
+      return handleSuccess(res, 200, "Employee fetch successfully", data);
+    } catch (error) {
+      console.error("Error registering Employee:", error);
+      return handleInternalServerError(res);
+    }
+  },
+  UpdateEmploys: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = await employee.findByIdAndUpdate(id, req.body);
+      if (!data) return handleError(res, 400, "Employee update fail");
+      return handleSuccess(res, 200, "Employee fetch successfully", data);
+    } catch (error) {
+      console.error("Error registering Employee:", error);
+      return handleInternalServerError(res);
+    }
+  },
+  DeleteEmploys: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = await employee.findByIdAndDelete(id);
+      if (!data) return handleError(res, 400, "Employee delete fail");
+      return handleSuccess(res, 200, "Employee delete successfully", data);
+    } catch (error) {
+      console.error("Error delete Employee:", error);
       return handleInternalServerError(res);
     }
   },
--- router/employesRouter.js
+++ router/employesRouter.js
@@ -3,5 +3,8 @@ const { employeControllers } = require("../controllers");
 const router = express.Router();
 
 router.post("/add", employeControllers.Add);
+router.put("/update/:id", employeControllers.UpdateEmploys);
+router.get("/get-all-employs", employeControllers.GetAllEmployee);
+router.delete("/delete/:id", employeControllers.DeleteEmploys);
 
 module.exports = router;
