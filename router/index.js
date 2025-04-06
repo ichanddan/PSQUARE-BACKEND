c33c2a99c81087fde6ad511fcc0130fbf632724c
chandan083 <code.us.cm@gmail.com>
2025-04-06 23:02:43 +0530

    Add token verification middleware to candidate, employes, leave, and attendance routes; update error handling in handler utility

--- router/index.js
+++ router/index.js
@@ -5,11 +5,12 @@ const candidateRouter = require("./candidateRouter");
 const employesRouter = require("./employesRouter");
 const leaveRouter = require("./leaveRouter");
 const attendanceRouter = require("./attendanceRouter");
+const verifyToken = require("../middleware/jwt");
 
 router.use("/auth", authRouter);
-router.use("/candidate", candidateRouter);
-router.use("/employs", employesRouter);
-router.use("/leave", leaveRouter);
-router.use("/attendance", attendanceRouter);
+router.use("/candidate",verifyToken ,candidateRouter);
+router.use("/employs", verifyToken,employesRouter);
+router.use("/leave", verifyToken,leaveRouter);
+router.use("/attendance",verifyToken ,attendanceRouter);
 
 module.exports = router;
